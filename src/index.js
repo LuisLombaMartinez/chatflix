d1af9268b821325d62fcea0044cbbe36e18370c2
Luis Lomba <luislombamartinez@gmail.com>
2023-07-25 14:25:11 +0200

    Added handling of exceptions and server closing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ const PORT = process.env.PORT || 8000;
 
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}.`);
+});
+
+// handle server errors
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info("Server closing.");
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+}
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// SIGTERM signal
+process.on("SIGTERM", () => {
+    logger.info("SIGTERM received.");
+    if (server) {
+        logger.info("Server closing.");
+        server.close();
+    }
 });
