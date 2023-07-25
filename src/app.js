ce72ac4c548f31172cab3e61af00cdbbba3bad43
Luis Lomba <luislombamartinez@gmail.com>
2023-07-25 12:52:13 +0200

    Added middleware dependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,55 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import fileUpload from 'express-fileupload';
+import cors from "cors";
+
+// dotEnv config
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from server!');
+// morgan
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"));
+}
+
+// helmet
+app.use(helmet());
+
+// parse JSON request url
+app.use(express.json());
+
+// parse JSON request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(mongoSanitize());
+
+// enable cookie-parser
+app.use(cookieParser());
+
+// gzip compression
+app.use(compression());
+
+// file upload
+app.use(fileUpload({
+    useTempFiles: true,
+}));
+
+// cors
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
+
+
+
+app.post('/test', (req, res) => {
+    res.send(`request received from ${req.body.name}`);
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
 import app from "./app.js";
-
-// env config
-dotenv.config();
 
 // env variables
 const PORT = process.env.PORT || 8000;
+
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
